068d3f7d717b5ad63e6d7b8f39c88a7d08cf448f
Rolandsson <102300415+Rolandsson@users.noreply.github.com>
2022-12-30 14:15:14 +0100

    Update index.js

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ app.use((req, res, next) => {
   res.header("Access-Control-Allow-Origin", corsOptions.origin);
   res.header("Access-Control-Allow-Headers", "Origin,X-Requested-With,Content-Type,Accept");
   res.header("Access-Control-Allow-Methods", "OPTIONS,GET,POST");
+    
+  if (req.method === "OPTIONS") {
+    return res.status(200).end();
+  }
+    
   next();
 });
 
